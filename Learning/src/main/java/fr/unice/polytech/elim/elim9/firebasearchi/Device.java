634e0368690418ba29ef4e2a3c89b13038c19c78
Michael Eusebe <michael.eusebe@etu.unice.fr>
2017-02-17 09:51:17 +0100

    Yolo

--- Learning/src/main/java/fr/unice/polytech/elim/elim9/firebasearchi/Device.java
+++ Learning/src/main/java/fr/unice/polytech/elim/elim9/firebasearchi/Device.java
@@ -1,5 +1,7 @@
 package fr.unice.polytech.elim.elim9.firebasearchi;
 
+import jdk.nashorn.internal.parser.JSONParser;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +9,12 @@ import java.util.Map;
  * Created by nathael on 17/02/17.
  */
 public class Device extends HashMap<String, Map<String,Double>> {
-        
+
+    public Device(){
+
+    }
+
+    public void feed(String json){
+    }
 
 }
